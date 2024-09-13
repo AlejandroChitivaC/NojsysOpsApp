ee5e970553ebf3b0fb4589a2d21595c2e9617209
achitivac <achitivac@outlook.com>
2024-09-13 09:31:01 -0500

    Changes on initial route

--- app/index.tsx
+++ app/index.tsx
@@ -22,7 +22,7 @@ const MainNavigator = () => {
 
   return (
     <AuthProvider>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Auth">
         <Stack.Screen
           name="Auth"
           component={Auth}
